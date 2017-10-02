acf8dff50eb0e85ce9346d79647a967136e52f05
Eloy <eloy.lopezce@gmail.com>
2017-10-02 00:54:57 -0400

    Add routing to index.js

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { BrowserRouter , Match, Miss } from 'react-router';
+
 import './css/style.css';
 import App from './components/App';
+
 import StorePicker from './components/StorePicker';
 
-render(<App/>, document.querySelector('#main'));
+const Root = () => {
+	return (
+		<BrowserRouter>
+			<div>
+				<Match exactly patttern="/" component={StorePicker} />
+				<Match patttern="/store/:storeId" component={App} />
+				<Miss component={notFound} />
+			</div>
+		</BrowserRouter>
+	)
+}
+
+render(<Root/>, document.querySelector('#main'));
