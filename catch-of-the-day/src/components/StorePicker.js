9f8332e5efc8cbed148d74a2a87147e979468ab6
Eloy <eloy.lopezce@gmail.com>
2017-10-02 23:44:12 -0400

    Add inventory state changes

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	goToStore(event) {
+		event.preventDefault();
+		console.log('you changed the store');
+		// first grab text from input, then transition from
+		const storeId = this.storeInput.value;
+		console.log(`Going to ${storeId}`);
+		// to store :storeId
+		this.context.router.transitionTo(`/store/${storeId}`);
+	}
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" />
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
 				<button type="submit">Visit Store</button>
 			</form>
 		)
 	}
 }
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
+}
+
 export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import {render} from 'react-dom';
-import { BrowserRouter , Match, Miss } from 'react-router';
+import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
 import './css/style.css';
 import App from './components/App';
-
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
 const Root = () => {
-	return (
-		<BrowserRouter>
-			<div>
-				<Match exactly patttern="/" component={StorePicker} />
-				<Match patttern="/store/:storeId" component={App} />
-				<Miss component={notFound} />
-			</div>
-		</BrowserRouter>
-	)
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
 }
 
 render(<Root/>, document.querySelector('#main'));
